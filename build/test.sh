982c9cea6b1d71cb432667ce5bd2cc4744345fa2
Eben Freeman <freemaneben@gmail.com>
2017-06-29 00:26:39 -0700

    Skip gofmt, vetting vendored directories in 'make test'
    go vet over all the vendored dependencies dredges up all kinds of crap

--- build/test.sh
+++ build/test.sh
@@ -20,24 +20,11 @@ set -o pipefail
 
 export CGO_ENABLED=0
 
-TARGETS=$(for d in "$@"; do echo ./$d/...; done)
+TARGETS=$(for d in "$@"; do go list ./$d/... | grep -v vendor ; done)
 
 echo "Running tests:"
 go test -i -installsuffix "static" ${TARGETS}
 go test -installsuffix "static" ${TARGETS}
-echo
-
-echo -n "Checking gofmt: "
-ERRS=$(find "$@" -type f -name \*.go | xargs gofmt -l 2>&1 || true)
-if [ -n "${ERRS}" ]; then
-    echo "FAIL - the following files need to be gofmt'ed:"
-    for e in ${ERRS}; do
-        echo "    $e"
-    done
-    echo
-    exit 1
-fi
-echo "PASS"
 echo
 
 echo -n "Checking go vet: "
