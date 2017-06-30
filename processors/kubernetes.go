1a114ddd3241e9385aadaf5695912c11b54dff03
Eben Freeman <freemaneben@gmail.com>
2017-06-30 14:11:54 -0700

    Pre-flatten k8s metadata that's added to events

--- processors/kubernetes.go
+++ processors/kubernetes.go
@@ -14,12 +14,68 @@ type KubernetesMetadataProcessor struct {
 }
 
 func (k *KubernetesMetadataProcessor) Process(data map[string]interface{}) {
-	podData, ok := k.PodGetter.Get(k.UID)
+	pod, ok := k.PodGetter.Get(k.UID)
 	if ok {
-		data["kubernetes.pod"] = podData
-		k.lastPodData = podData
+		k.lastPodData = pod
 	} else if k.lastPodData != nil {
-		data["kubernetes.pod"] = k.lastPodData
+		pod = k.lastPodData
 	}
 	// TODO add container metadata too
+
+	if pod != nil {
+		metadata := extractMetadataFromPod(pod, k.ContainerName)
+		for k, v := range metadata {
+			data["kubernetes."+k] = v
+		}
+	}
+}
+
+type PodMetadata struct {
+	Labels             map[string]string
+	Name               string
+	Namespace          string
+	ResourceVersion    string
+	UID                string
+	NodeName           string
+	NodeSelector       map[string]string
+	ServiceAccountName string
+	Subdomain          string
+}
+
+type ContainerMetadata struct {
+	Args         string
+	Command      string
+	Env          string
+	Image        string
+	Name         string
+	Ports        string
+	Resources    string
+	VolumeMounts string
+	WorkingDir   string
+}
+
+func extractMetadataFromPod(pod *v1.Pod, containerName string) map[string]interface{} {
+	ret := make(map[string]interface{})
+	ret["pod.labels"] = pod.Labels
+	ret["pod.name"] = pod.Name
+	ret["pod.namespace"] = pod.Namespace
+	ret["pod.resourceVersion"] = pod.ResourceVersion
+	ret["pod.UID"] = string(pod.UID)
+	ret["pod.nodeName"] = pod.Spec.NodeName
+	ret["pod.nodeSelector"] = pod.Spec.NodeSelector
+	ret["pod.serviceAccountName"] = pod.Spec.ServiceAccountName
+	ret["pod.subdomain"] = pod.Spec.Subdomain
+
+	for _, container := range pod.Spec.Containers {
+		if container.Name == containerName {
+			ret["container.args"] = container.Args
+			ret["container.command"] = container.Command
+			ret["container.env"] = container.Env
+			ret["container.image"] = container.Image
+			ret["container.ports"] = container.Ports
+			ret["container.VolumeMounts"] = container.VolumeMounts
+			ret["container.workingDir"] = container.WorkingDir
+		}
+	}
+	return ret
 }
