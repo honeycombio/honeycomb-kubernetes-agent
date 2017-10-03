5dfa3ce07fc6ba3cf2d81a80a8d000012b45acba
Eben Freeman <eben@honeycomb.io>
2017-10-03 11:59:53 -0700

    Pin Kubernetes version in end-to-end testing to avoid regression in v1.8
    See https://github.com/kubernetes/kubernetes/issues/53309

--- e2e-tests/minikube-bootstrap.sh
+++ e2e-tests/minikube-bootstrap.sh
@@ -3,8 +3,8 @@
 # From https://github.com/kubernetes/minikube
 # Minikube setup for CI -- don't use this locally, use the normal install
 # instructions for minikube and kubectl instead
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.21.0/minikube-linux-amd64 && chmod +x minikube
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.22.2/minikube-linux-amd64 && chmod +x minikube
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.7.5/bin/linux/amd64/kubectl && chmod +x kubectl
 
 export MINIKUBE_WANTUPDATENOTIFICATION=false
 export MINIKUBE_WANTREPORTERRORPROMPT=false
@@ -14,7 +14,7 @@ mkdir $HOME/.kube || true
 touch $HOME/.kube/config
 
 export KUBECONFIG=$HOME/.kube/config
-sudo -E ./minikube start --vm-driver=none
+sudo -E ./minikube start --vm-driver=none --kubernetes-version v1.7.5
 
 # this for loop waits until kubectl can access the api server that minikube has created
 for i in {1..150} # timeout for 5 minutes
