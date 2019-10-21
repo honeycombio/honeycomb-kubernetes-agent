704de38bb8918624f860ab13a988d8a2729be9da
D. Spindel <spider@skuggor.se>
2019-10-21 18:27:28 +0200

    processors: Add a pair of simple filter processors (#52)
    * processors: Add a pair of simple filter processors
    
    This filters events based on exact matches of key:values, in the form of two
    basic processors.
    
    keep_event:
      Matches a single key to one of a series of values, and
      keeps only events matching this value.
    
    drop_event:
      Matches a single key to one of a series of values, and drops all events matching
      this value
    
    These are intended as processor filters from things like the `nginx-ingress`
    which contains data from one or more datasets, which should be split out based
    on some pre-configured conditions.
    
    Typical usecase was to filter on the k8s.service or a specific hostname.
    
    It only performs exact string matches, as it is not intended to be a generic
    filter or routing implementation.
    
    * processors: Fix drop_event and keep_event after first review step
    
    Review by @seh, comments in [0]
    
    - Add proper type in debug output
    - Use a set-like map instead of a map of booleans
    - Slim down the nesting depth in Process by exiting early.
    
    [0] https://github.com/honeycombio/honeycomb-kubernetes-agent/pull/52
    
    * docs: Document drop_event and keep_event processors
    
    A basic documentation similar to the other one, hopefully giving
    enough background for how to use it sort of effectively.
    
    * fixup! processors: Fix drop_event and keep_event after first review step
    
    * fixup! docs: Document drop_event and keep_event processors
    
    * fixup! fixup! processors: Fix drop_event and keep_event after first review step

---
+++ processors/drop_event.go
@@ -0,0 +1,63 @@
+package processors
+
+import (
+	"errors"
+	"fmt"
+	"github.com/honeycombio/honeycomb-kubernetes-agent/event"
+	"github.com/mitchellh/mapstructure"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	ErrFilterOptionUnspecified = errors.New("drop_event processor requires a 'Field' to be set")
+)
+
+type EventDropper struct {
+	config *eventDropperConfig
+	values map[string]struct{}
+}
+
+type eventDropperConfig struct {
+	Field  string
+	Values []string
+}
+
+func (f *EventDropper) Init(options map[string]interface{}) error {
+	config := &eventDropperConfig{}
+	err := mapstructure.Decode(options, config)
+	if err != nil {
+		return err
+	}
+
+	if config.Field == "" {
+		return ErrFilterOptionUnspecified
+	}
+	f.config = config
+
+	f.values = make(map[string]struct{}, len(f.config.Values))
+	for _, val := range f.config.Values {
+		f.values[val] = struct{}{}
+	}
+	return nil
+}
+
+func (f *EventDropper) Process(ev *event.Event) bool {
+	if ev.Data == nil {
+		return true
+	}
+	val, ok := ev.Data[f.config.Field]
+	if !ok {
+		return true
+	}
+	valString, ok := val.(string)
+	if !ok {
+		logrus.WithFields(logrus.Fields{
+			"key":   f.config.Field,
+			"value": val,
+			"type":  fmt.Sprintf("%T", val)}).
+			Debug("Not filtering field of non-string type")
+		return true
+	}
+	_, exists := f.values[valString]
+	return !exists
+}
---
+++ processors/keep_event.go
@@ -0,0 +1,61 @@
+package processors
+
+import (
+	"fmt"
+	"github.com/honeycombio/honeycomb-kubernetes-agent/event"
+	"github.com/mitchellh/mapstructure"
+	"github.com/sirupsen/logrus"
+)
+
+type EventKeeper struct {
+	config *eventKeeperConfig
+	values map[string]struct{}
+}
+
+type eventKeeperConfig struct {
+	Field  string
+	Values []string
+}
+
+func (f *EventKeeper) Init(options map[string]interface{}) error {
+	config := &eventKeeperConfig{}
+	err := mapstructure.Decode(options, config)
+	if err != nil {
+		return err
+	}
+
+	if config.Field == "" {
+		return ErrFilterOptionUnspecified
+	}
+	f.config = config
+
+	f.values = make(map[string]struct{}, len(f.config.Values))
+	for _, val := range f.config.Values {
+		f.values[val] = struct{}{}
+	}
+	return nil
+}
+
+func (f *EventKeeper) Process(ev *event.Event) bool {
+	if ev.Data == nil {
+		return true
+	}
+
+	val, ok := ev.Data[f.config.Field]
+	if !ok {
+		return true
+	}
+
+	valString, ok := val.(string)
+	if !ok {
+		logrus.WithFields(logrus.Fields{
+			"key":   f.config.Field,
+			"value": val,
+			"type":  fmt.Sprintf("%T", val),
+		}).
+			Debug("Not filtering field of non-string type")
+		return true
+	}
+	_, exists := f.values[valString]
+	return exists
+}
