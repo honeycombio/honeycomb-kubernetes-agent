106d9454b84425215f38612d1bb2bf442b01a2b7
Pierre Tessier <pierre@pierretessier.com>
2020-09-28 15:57:27 -0400

    unit tests for metadata

--- metrics/processor_test.go
+++ metrics/processor_test.go
@@ -2,39 +2,35 @@ package metrics
 
 import (
 	"github.com/honeycombio/honeycomb-kubernetes-agent/kubelet"
+	"github.com/honeycombio/honeycomb-kubernetes-agent/metrics/mock"
 	"github.com/sirupsen/logrus"
-	"io/ioutil"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-type fakeRestClient struct {
-}
-
-func (f fakeRestClient) StatsSummary() ([]byte, error) {
-	return ioutil.ReadFile("../testdata/stats-summary.json")
-}
-
-func (f fakeRestClient) Pods() ([]byte, error) {
-	return ioutil.ReadFile("../testdata/pods.json")
-}
-
-func TestMetricAccumulator(t *testing.T) {
-	rc := &fakeRestClient{}
+func TestGenerateMetricsData(t *testing.T) {
+	rc := &metrics_mock.MockRestClient{}
 	statsProvider := kubelet.NewStatsProvider(rc)
 	summary, _ := statsProvider.StatsSummary()
 	metadataProvider := kubelet.NewMetadataProvider(rc)
 	podsMetadata, _ := metadataProvider.Pods()
 	metadata := NewMetadata(podsMetadata, nil, logrus.StandardLogger())
 
-	// Disable all groups
 	p := NewMetricsProcessor(10*time.Second, logrus.StandardLogger())
-	require.Equal(t, 28, len(p.GenerateMetricsData(summary, metadata, map[MetricGroup]bool{
+
+	require.Equal(t, 42, len(p.GenerateMetricsData(summary, metadata, map[MetricGroup]bool{
 		"node":      true,
 		"pod":       true,
 		"container": true,
 		"volume":    true,
+	})))
+
+	require.Equal(t, 12, len(p.GenerateMetricsData(summary, metadata, map[MetricGroup]bool{
+		"node":      true,
+		"pod":       true,
+		"container": false,
+		"volume":    false,
 	})))
 }
