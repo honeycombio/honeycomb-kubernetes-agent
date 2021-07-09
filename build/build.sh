c9c04468984ab32f7efa2eb063c1d1c328bcdc3b
Ian Smith <iansmith@honeycomb.io>
2021-07-08 19:04:16 -0700

    convert to ko

--- build/build.sh
+++ build/build.sh
@@ -1,43 +1,19 @@
 #!/bin/bash
 
-# Copyright 2016 The Kubernetes Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 set -o errexit
 set -o nounset
 set -o pipefail
+set -x
 
-if [ -z "${PKG}" ]; then
-    echo "PKG must be set"
-    exit 1
-fi
-if [ -z "${BIN}" ]; then
-    echo "BIN must be set"
-    exit 1
-fi
-if [ -z "${ARCH}" ]; then
-    echo "ARCH must be set"
-    exit 1
-fi
 if [ -z "${VERSION}" ]; then
     echo "VERSION must be set"
     exit 1
 fi
 
 export CGO_ENABLED=0
-export GOARCH="${ARCH}"
 
-go build -o ./bin/${ARCH}/${BIN} \
-    -ldflags "-X ${PKG}/version.VERSION=${VERSION}" \
-    .
+export KO_DOCKER_REPO=${KO_DOCKER_REPO:-ko.local}
+# See https://github.com/google/ko for options
+# --base-import-paths avoids us having a weird -$(md5sum) at the end of the
+# image name
+ko publish --tags head,${VERSION} --base-import-paths --platform=linux/amd64,linux/arm64 .
