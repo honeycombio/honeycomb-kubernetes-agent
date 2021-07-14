444c4499db9b4be780da05467b15bc863f1f12ed
Ian Smith <iansmith@honeycomb.io>
2021-07-14 13:44:12 -0700

    use ko to build instead of make

--- build/build.sh
+++ build/build.sh
@@ -18,26 +18,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ -z "${PKG}" ]; then
-    echo "PKG must be set"
-    exit 1
-fi
-if [ -z "${BIN}" ]; then
-    echo "BIN must be set"
-    exit 1
-fi
-if [ -z "${ARCH}" ]; then
-    echo "ARCH must be set"
-    exit 1
-fi
-if [ -z "${VERSION}" ]; then
-    echo "VERSION must be set"
-    exit 1
-fi
-
-export CGO_ENABLED=0
-export GOARCH="${ARCH}"
-
-go build -o ./bin/${ARCH}/${BIN} \
-    -ldflags "-X ${PKG}/version.VERSION=${VERSION}" \
-    .
+ko publish --local \
+  --tags "head,$(cat "$(readlink -f "$0")")" \
+  --base-import-paths \
+  --platform=linux/amd64,linux/arm64 \
+  .
